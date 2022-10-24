e7f00b78091643a9f99fb5aac7ae321148b54d08
Arif Babayev <arifrb@code.edu.az>
2022-10-24 15:40:15 +0400

    React Icons fix

--- src/App.js
+++ src/App.js
@@ -8,21 +8,8 @@ import "./App.css";
 import { Navbar, Sidebar, ThemeSettings } from "./components";
 import {
   Ecommerce,
-  Orders,
-  Calendar,
-  Employees,
-  Stacked,
-  Pyramid,
-  Customers,
-  Kanban,
-  Area,
-  Line,
-  Bar,
-  Pie,
-  Financial,
-  ColorPicker,
-  ColorMapping,
-  Editor,
+  Employees
+  
 } from "./pages";
 
 import { useStateContext } from "./contexts/ContextProvider";
@@ -82,27 +69,8 @@ const App = () => {
                 {/* Dashboard */}
                 <Route path="/" element={<Ecommerce />} />
                 <Route path="/ecommerce" element={<Ecommerce />} />
-
-                {/* Pages */}
-                <Route path="/orders" element={<Orders />} />
                 <Route path="/employees" element={<Employees />} />
-                <Route path="/customers" element={<Customers />} />
-
-                {/* Apps */}
-                <Route path="/kanban" element={<Kanban />} />
-                <Route path="/editor" element={<Editor />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/color-picker" element={<ColorPicker />} />
-
-                {/* Charts */}
-                <Route path="/line" element={<Line />} />
-                <Route path="/area" element={<Area />} />
-                <Route path="/bar" element={<Bar />} />
-                <Route path="/pie" element={<Pie />} />
-                <Route path="/financial" element={<Financial />} />
-                <Route path="/color-mapping" element={<ColorMapping />} />
-                <Route path="/pyramid" element={<Pyramid />} />
-                <Route path="/stacked" element={<Stacked />} />
+             
               </Routes>
             </div>
           </div>
